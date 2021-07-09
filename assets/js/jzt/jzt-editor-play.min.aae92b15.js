79f6c2da36668a20c2e58890ca2f6c97b0621dae
Mark McIntyre <mark@markmcintyre.ca>
2021-07-08 21:31:16 -0400

    Release 1.0.56

--- assets/js/jzt/jzt-editor-play.min.aae92b15.js
+++ assets/js/jzt/jzt-editor-play.min.aae92b15.js
@@ -4,7 +4,7 @@
  * Created by Mark McIntyre
  * All rights reserved.
  * 
- * Version : 1.0.55
- * Released: 2021-04-24
+ * Version : 1.0.56
+ * Released: 2021-07-09
  */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var n;n="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:this,n.jztPlayer=e()}}(function(){return function(){function e(n,t,o){function r(a,f){if(!t[a]){if(!n[a]){var d="function"==typeof require&&require;if(!f&&d)return d(a,!0);if(i)return i(a,!0);var u=new Error("Cannot find module '"+a+"'");throw u.code="MODULE_NOT_FOUND",u}var l=t[a]={exports:{}};n[a][0].call(l.exports,function(e){var t=n[a][1][e];return r(t||e)},l,l.exports,e,n,t,o)}return t[a].exports}for(var i="function"==typeof require&&require,a=0;a<o.length;a++)r(o[a]);return r}return e}()({1:[function(e,n,t){"use strict";function o(e){var n=document.querySelector("button");e.origin===a&&e.data&&e.data.lastIndexOf&&0===e.data.lastIndexOf("play-game:",0)&&(n.innerText="Click to play",n.addEventListener("click",function(){n.remove(),i=new jzt.Game({canvasElement:document.getElementById("jzt"),playTest:!0,onLoadCallback:function(n){n&&this.run(JSON.parse(e.data.substring(10)))}})}))}function r(){a=window.location.origin||window.location.protocol+"//"+window.location.host,window.addEventListener("message",o,!1),window.opener.postMessage("send-game",a)}var i,a;r()},{}]},{},[1])(1)});
